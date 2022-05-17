83ebf4beda8945c3c0afc0758eb16ae3c61dab50
Tristan <1286511095@qq.com>
2022-05-17 20:58:53 +0800

    1

---
+++ obj.js
@@ -0,0 +1,37 @@
+class User {
+    constructor() {
+        window.addEventListener('message',function(e){
+            console.log("receive data：" + e.data.toString());
+            alert("receive data：" + e.data.toString());
+            this.token = e.data.toString();
+        });
+    }
+    getToken() {
+        window.top.postMessage(
+            {
+                msg:'get token',
+                type: 2
+            },
+            "file://*"
+        );
+    }
+
+    closeWindow() {
+        window.top.postMessage(
+            {
+                msg:'close webview',
+                type: 1
+            },
+            "file://*"
+        );
+    }
+    removeToken() {
+        window.top.postMessage(
+            {
+                msg:'remove token',
+                type: 3
+            },
+            "file://*"
+        );
+    }
+}
