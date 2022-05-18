064b10cc3b1d8422c5b5352cb7777dd3f5bd9636
Tristan <1286511095@qq.com>
2022-05-18 16:06:39 +0800

    1

--- injectScript.js
+++ injectScript.js
@@ -1,49 +1,41 @@
-class Bridge {
-    constructor() {
-        window.addEventListener('message',function(e){
-            console.log("receive data：" + e.data.toString());
-            alert("receive data：" + e.data.toString());
-        });
-    }
-
-    getToken() {
-        window.top.postMessage(
-            {
-                msg:'get token',
-                type: 2
-            },
-            "file://*"
-        );
-    }
-
-    closeWindow() {
-        window.top.postMessage(
-            {
-                msg:'close webview',
-                type: 1
-            },
-            "file://*"
-        );
-    }
-
-    removeToken() {
-        window.top.postMessage(
-            {
-                msg:'remove token',
-                type: 3
-            },
-            "file://*"
-        );
-    }
+function getToken() {
+    let token = '';
+    window.top.postMessage(
+        {
+            msg:'get token',
+            type: 2
+        },
+        "file://*"
+    );
+    return token;
 }
+
+function closeWindow() {
+    window.top.postMessage(
+        {
+            msg:'close webview',
+            type: 1
+        },
+        "file://*"
+    );
+}
+
+function removeToken() {
+    window.top.postMessage(
+        {
+            msg:'remove token',
+            type: 3
+        },
+        "file://*"
+    );
+}
+
+const handelMessage = (e) => {
+    console.log("receive data：" + e.data.toString());
+    alert("receive data：" + e.data.toString());
+}
+
 window.onload = function(){
-    // window.NativeBridge = new Bridge();
-    window.addEventListener('message',function(e){
-        console.log("receive data：" + e.data.toString());
-        if (e.data.type === 1){
-            document.getElementsByTagName('head')[0].append(e.data.data);
-        }
-        // alert("receive data：" + e.data.toString());
-    });
+    window.onmessage = handelMessage;
 }
 
