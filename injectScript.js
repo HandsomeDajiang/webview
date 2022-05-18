1f6af2411cb11190789a768feba4840e8ec9cd52
Tristan <1286511095@qq.com>
2022-05-18 15:30:43 +0800

    1

--- injectScript.js
+++ injectScript.js
@@ -1,4 +1,4 @@
-class User {
+class Bridge {
     constructor() {
         window.addEventListener('message',function(e){
             console.log("receive data：" + e.data.toString());
@@ -37,6 +37,13 @@ class User {
     }
 }
 window.onload = function(){
-    window.NativeBridge = new User();
+    // window.NativeBridge = new Bridge();
+    window.addEventListener('message',function(e){
+        console.log("receive data：" + e.data.toString());
+        if (e.data.type === 1){
+            document.getElementsByTagName('head')[0].append(e.data.data);
+        }
+        // alert("receive data：" + e.data.toString());
+    });
 }
 
