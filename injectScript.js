a07e8bd418a93acd5d6401f5becdbb8d8894f988
Tristan <1286511095@qq.com>
2022-05-18 20:56:54 +0800

    1

--- injectScript.js
+++ injectScript.js
@@ -1,41 +1,42 @@
-// function getToken() {
-//     window.top.postMessage(
-//         {
-//             msg:'get token',
-//             type: 2
-//         },
-//         "file://*"
-//     );
-//     return window.token;
-// }
-//
-// function closeWindow() {
-//     window.top.postMessage(
-//         {
-//             msg:'close webview',
-//             type: 1
-//         },
-//         "file://*"
-//     );
-// }
-//
-// function removeToken() {
-//     window.top.postMessage(
-//         {
-//             msg:'remove token',
-//             type: 3
-//         },
-//         "file://*"
-//     );
-// }
-//
-// const handelMessage = (e) => {
-//     window.token = e.data.toString();
-//     console.log("receive data：" + e.data.toString());
-//     alert("receive data：" + e.data.toString());
-// }
-//
-// window.onload = function(){
-//     window.onmessage = handelMessage;
-// }
+async function getToken() {
+    return new Promise((resolve, reject) => {
+        window.top.postMessage(
+            {
+                msg:'get token',
+                type: 2
+            },
+            "file://*"
+        );
+
+    })
+}
+
+function closeWindow() {
+    window.top.postMessage(
+        {
+            msg:'close webview',
+            type: 1
+        },
+        "file://*"
+    );
+}
+
+function removeToken() {
+    window.top.postMessage(
+        {
+            msg:'remove token',
+            type: 3
+        },
+        "file://*"
+    );
+}
+
+const handelMessage = (e) => {
+    console.log("receive data：" + e.data.toString());
+    alert("receive data：" + e.data.toString());
+}
+
+window.onload = function(){
+    window.onmessage = handelMessage;
+}
 
