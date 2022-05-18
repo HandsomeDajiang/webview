6530f02dda05504c0485d8e2e9387d38afffaee7
Tristan <1286511095@qq.com>
2022-05-18 15:11:32 +0800

    1

--- injectScript.js
+++ injectScript.js
@@ -1,43 +1,42 @@
-// class User {
-//     constructor() {
-//         window.addEventListener('message',function(e){
-//             console.log("receive data：" + e.data.toString());
-//             alert("receive data：" + e.data.toString());
-//             this.token = e.data.toString();
-//         });
-//     }
-//
-//     getToken() {
-//         window.top.postMessage(
-//             {
-//                 msg:'get token',
-//                 type: 2
-//             },
-//             "file://*"
-//         );
-//     }
-//
-//     closeWindow() {
-//         window.top.postMessage(
-//             {
-//                 msg:'close webview',
-//                 type: 1
-//             },
-//             "file://*"
-//         );
-//     }
-//
-//     removeToken() {
-//         window.top.postMessage(
-//             {
-//                 msg:'remove token',
-//                 type: 3
-//             },
-//             "file://*"
-//         );
-//     }
-// }
-// window.onload = function(){
-//     window.utils = new User();
-// }
-//
+class User {
+    constructor() {
+        window.addEventListener('message',function(e){
+            console.log("receive data：" + e.data.toString());
+            alert("receive data：" + e.data.toString());
+        });
+    }
+
+    getToken() {
+        window.top.postMessage(
+            {
+                msg:'get token',
+                type: 2
+            },
+            "file://*"
+        );
+    }
+
+    closeWindow() {
+        window.top.postMessage(
+            {
+                msg:'close webview',
+                type: 1
+            },
+            "file://*"
+        );
+    }
+
+    removeToken() {
+        window.top.postMessage(
+            {
+                msg:'remove token',
+                type: 3
+            },
+            "file://*"
+        );
+    }
+}
+window.onload = function(){
+    window.NativeBridge = new User();
+}
+
