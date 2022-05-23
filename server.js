d842b01b665aeb2b35342d92d9bb049686690223
Tristan <1286511095@qq.com>
2022-05-23 10:18:17 +0800

    1

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ async function getWebToken() {
 
 async function getToken() {
     const token = await getWebToken();
-    document.getElementById('text').innerHTML = token.result.data.toString();
+    console.log(token);
+    document.getElementById('text').innerHTML = token.toString();
 }
 
 const handelMessage = (e) => {
@@ -27,6 +28,6 @@ const handelMessage = (e) => {
 
 window.onload = function(){
     window.onmessage = handelMessage; // 监听消息
-    window.Callbacks = {} // 全局回调对象，  key：随机id ， value：回调函数
+    // window.Callbacks = {} // 全局回调对象，  key：随机id ， value：回调函数
 }
 
