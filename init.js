63e8e0ec9796d140026cc51cf8596cbe9186ad94
Tristan <1286511095@qq.com>
2022-05-23 15:06:17 +0800

    1

---
+++ init.js
@@ -0,0 +1,59 @@
+class Bridge {
+    callHandler(method,params,callback){
+       // window.WKWVJBCallbacks[method](params)  返回 promise
+        callback(window.WKWVJBCallbacks[method](params));
+    }
+}
+const bridge = new Bridge();
+
+const getMiniProgramToken = async function(params) {
+    return await getToken(params);
+}
+
+const  getToken = async function(params) {
+    window.miniProgramToken = undefined;
+    const timestamp = new Date().getTime().toString();
+    window.WKWVJBTempCallbacks[timestamp] = _getToken;
+    window.top.postMessage(
+        {
+            type: '2',
+            callbackid: timestamp,
+            params
+        },
+        "file://*"
+    );
+    return new Promise((resolve)=>{
+        setInterval(()=>{
+            if (window.miniProgramToken){
+                clearInterval();
+                resolve(window.miniProgramToken);
+            }
+        },50);
+    });
+}
+
+const _getToken = function(response) {
+    window.miniProgramToken = response;
+}
+
+const handelMessage = function(e) {
+    const callbackid = e.data.callbackid;
+    if (callbackid){
+        window.WKWVJBTempCallbacks[callbackid](e.data.response);
+    }
+
+    // 执行完回调后清空 WKWVJBTempCallbacks 避免无限添加。
+    window.WKWVJBTempCallbacks = {}
+}
+
+
+window.onmessage = handelMessage;
+window.WKWVJBCallbacks['getMiniProgramToken'] = getMiniProgramToken;    // 注册方法并绑定到 window 全局对象上。
+
+this.setupWKWebViewJavascriptBridge = (function(){
+    let parms = {'appId': 'b4933e7b0c12f9c16a'}
+    bridge.callHandler('getMiniProgramToken', parms, function(response) {
+        console.log("$$$$$$$$");
+        console.log(response);
+    })
+})
--- server.js
+++ server.js
@@ -1,92 +1,93 @@
-
-function closeWindow() {
-    window.top.postMessage(
-        {
-            type: '1',
-        },
-        "file://*"
-    );
-}
-
-async function getToken() {
-    window.miniProgramToken = undefined;
-    const timestamp = new Date().getTime().toString();
-    window.WKWVJBCallbacks[timestamp] = _getToken;
-    window.top.postMessage(
-        {
-            type: '2',
-            callbackid: timestamp
-        },
-        "file://*"
-    );
-    return new Promise((resolve)=>{
-        setInterval(()=>{
-            if (window.miniProgramToken){
-                clearInterval();
-                resolve(window.miniProgramToken);
-            }
-        },50);
-    });
-}
-
-async function getMiniProgramToken() {
-    const token = await getToken();
-    console.log(token);
-    document.getElementById('text').innerHTML = token.toString();
-}
-
-const _getToken = (response) => {
-    window.miniProgramToken = response;
-}
-const _removeToken = (response) => {
-    window.miniProgramTokenRemove = response;
-}
-
-async function removeMiniProgramToken() {
-    const result = await removeToken();
-    console.log(result.toString());
-    document.getElementById('text').innerHTML = result.toString();
-}
-
-async function removeToken() {
-    window.miniProgramTokenRemove = undefined;
-    const timestamp = new Date().getTime().toString();
-    window.WKWVJBCallbacks[timestamp] = _removeToken;
-    window.top.postMessage(
-        {
-            type: '3',
-            callbackid: timestamp
-        },
-        "file://*"
-    );
-    return new Promise((resolve)=>{
-        setInterval(()=>{
-            if (window.miniProgramTokenRemove){
-                clearInterval();
-                resolve(window.miniProgramTokenRemove);
-            }
-        },50);
-    });
-}
-
-const handelMessage = (e) => {
-    const callbackid = e.data.callbackid;
-    if (callbackid){
-        window.WKWVJBCallbacks[callbackid](e.data.response);
-    }
-}
-
-window.onload = function(){
-    window.onmessage = handelMessage; // 监听消息
-
-    //清空一下，避免无限添加。
-    window.WKWVJBCallbacks = {} // 全局回调对象，  key：随机id ， value：回调函数
-
-    window.WKWebViewJavascriptBridge = {
-
-    }
-
-}
-
-
-
+//
+// function closeWindow() {
+//     window.top.postMessage(
+//         {
+//             type: '1',
+//         },
+//         "file://*"
+//     );
+// }
+//
+// async function getToken() {
+//     window.miniProgramToken = undefined;
+//     const timestamp = new Date().getTime().toString();
+//     window.WKWVJBCallbacks[timestamp] = _getToken;
+//     window.top.postMessage(
+//         {
+//             type: '2',
+//             callbackid: timestamp
+//         },
+//         "file://*"
+//     );
+//     return new Promise((resolve)=>{
+//         setInterval(()=>{
+//             if (window.miniProgramToken){
+//                 clearInterval();
+//                 resolve(window.miniProgramToken);
+//             }
+//         },50);
+//     });
+// }
+//
+// async function getMiniProgramToken() {
+//     const token = await getToken();
+//     console.log(token);
+//     document.getElementById('text').innerHTML = token.toString();
+// }
+//
+// const _getToken = (response) => {
+//     window.miniProgramToken = response;
+// }
+// const _removeToken = (response) => {
+//     window.miniProgramTokenRemove = response;
+// }
+//
+// async function removeMiniProgramToken() {
+//     const result = await removeToken();
+//     console.log(result.toString());
+//     document.getElementById('text').innerHTML = result.toString();
+// }
+//
+// async function removeToken() {
+//     window.miniProgramTokenRemove = undefined;
+//     const timestamp = new Date().getTime().toString();
+//     window.WKWVJBCallbacks[timestamp] = _removeToken;
+//     window.top.postMessage(
+//         {
+//             type: '3',
+//             callbackid: timestamp
+//         },
+//         "file://*"
+//     );
+//     return new Promise((resolve)=>{
+//         setInterval(()=>{
+//             if (window.miniProgramTokenRemove){
+//                 clearInterval();
+//                 resolve(window.miniProgramTokenRemove);
+//             }
+//         },50);
+//     });
+// }
+//
+// const handelMessage = (e) => {
+//     const callbackid = e.data.callbackid;
+//     if (callbackid){
+//         window.WKWVJBCallbacks[callbackid](e.data.response);
+//     }
+// }
+//
+// window.onload = function(){
+//     if (!window.WKWebViewJavascriptBridge) {
+//         return callback(window.WKWebViewJavascriptBridge);
+//     }
+//     if (window.WKWebViewJavascriptBridge) {
+//         return callback(window.WKWebViewJavascriptBridge);
+//     }
+//     if (window.WKWVJBCallbacks) {
+//         return window.WKWVJBCallbacks.push(callback);
+//     }
+// }
+//
+//
+//
+//
