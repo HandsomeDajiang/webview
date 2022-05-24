ba5147f9852236aa0f156cf081f170bf4aea23ce
Tristan <1286511095@qq.com>
2022-05-24 11:51:51 +0800

    1

--- init.js
+++ init.js
@@ -1,76 +1,73 @@
-const TARGET_ORIGIN = "file://*";
-window.onmessage = handelMessage;
-window.WKWVJBCallbacks = {}
+export function inject() {
+    const TARGET_ORIGIN = "file://*";
+    window.onmessage = handelMessage;
+    window.WKWVJBCallbacks = {}
 
-window.WKWebViewJavascriptBridge = {
-    callHandler: callHandler,
-};
+    window.WKWebViewJavascriptBridge = {
+        callHandler: callHandler,
+    };
 
-async function callHandler(methodName, params, callback) {
-    callback(await postmessage(methodName,params));
-}
-
-function setupWKWebViewJavascriptBridge(callback) {
-    if (window.WKWebViewJavascriptBridge) {
-        return callback(window.WKWebViewJavascriptBridge);
-    }
-    return null;
-}
-
-async function postmessage(methodName, params) {
-    window.WKWVJBCallbacks = {};
-    const callbackid = new Date().getTime().toString();
-    console.log("进入postmessage方法");
-    const message = {
-        params,
-        callbackid,
-        methodName,
+    async function callHandler(methodName, params, callback) {
+        callback(await postmessage(methodName,params));
     }
 
-    window.top.postMessage(
-        message,
-        TARGET_ORIGIN
-    );
-    console.log("postmessage发送了消息");
-    return new Promise((resolve)=>{
-        const timer = setInterval(()=>{
-            if (window.WKWVJBCallbacks[callbackid]){
-                clearInterval(timer);
-                console.log("有数据了！");
-                resolve(window.WKWVJBCallbacks[callbackid]);
-            }
-        },100);
-    });
-}
+    function setupWKWebViewJavascriptBridge(callback) {
+        if (window.WKWebViewJavascriptBridge) {
+            return callback(window.WKWebViewJavascriptBridge);
+        }
+        return null;
+    }
 
-function responseTempOperation(response, callbackid) {
-    console.log("处理回调");
-    window.WKWVJBCallbacks[callbackid] = response;
-    console.log("存储的数据为:" + window.WKWVJBCallbacks[callbackid])
-}
+    async function postmessage(methodName, params) {
+        window.WKWVJBCallbacks = {};
+        const callbackid = new Date().getTime().toString();
+        const message = {
+            params,
+            callbackid,
+            methodName,
+        }
 
-function handelMessage(e) {
-    console.log("收到了回调");
-    console.log(e.data);
-    const { callbackid, response } = e.data || {}
-    const { status } = response || {}
-    if (status && status === 400) {
+        window.top.postMessage(
+            message,
+            TARGET_ORIGIN
+        );
+        return new Promise((resolve)=>{
+            const timer = setInterval(()=>{
+                if (window.WKWVJBCallbacks[callbackid]){
+                    clearInterval(timer);
+                    resolve(window.WKWVJBCallbacks[callbackid]);
+                }
+            },100);
+        });
+    }
+
+    function responseTempOperation(response, callbackid) {
+        window.WKWVJBCallbacks[callbackid] = response;
+    }
+
+    function handelMessage(e) {
+        const { callbackid, response } = e.data || {}
+        const { status } = response || {}
+        if (status && status === 400) {
+            clearInterval();
+            alert('error');
+            return
+        }
+
+        if (callbackid){
+            responseTempOperation(response, callbackid);
+            return;
+        }
+
         clearInterval();
-        alert('error');
-        return
     }
-
-    if (callbackid){
-        responseTempOperation(response, callbackid);
-    }
-
 }
 
 function getToken() {
     this.setupWKWebViewJavascriptBridge(function (bridge) {
         let params = {'appId': 'b4933e7b0c12f9c16a'}
         bridge.callHandler('getMiniProgramToken', params, function(response) {
-            console.log("回来的数据：" + response)
+            console.log("回来的数据：" + response);
             if (response) {
                 console.log("##########");
                 console.log(response);
@@ -101,6 +98,5 @@ function closeWindow() {
         });
     });
 }
-
 
 
