15eaa0805d86663d7ca2138726976011f859a5ca
Tristan <1286511095@qq.com>
2022-05-24 14:46:44 +0800

    1

--- demo.js
+++ demo.js
@@ -58,64 +58,64 @@
 //     return null;
 // }
 
+const TARGET_ORIGIN = "file://*";
+function macOsInjectWKWebViewJavascriptBridge() {
+    window.WKWVJBCallbacks = {}
+    window.onmessage = handelMessage;
+    window.WKWebViewJavascriptBridge = {
+        callHandler: callHandler,
+    };
+    window.setupWKWebViewJavascriptBridge = setupWKWebViewJavascriptBridge
+}
+function handelMessage(e){
+    const { callbackid, response } = e.data || {}
+    const { status } = response || {}
+    if (status && status === 400) {
+        clearInterval();
+        alert('error');
+        return
+    }
 
-class Bridge {
-    constructor() {
-        window.WKWVJBCallbacks = {}
-        window.WKWebViewJavascriptBridge = {
-            callHandler: this.callHandler,
-        };
-        window.TARGET_ORIGIN =  "file://*";
-        window.onmessage = this.handelMessage;
-        window.setupWKWebViewJavascriptBridge = this.setupWKWebViewJavascriptBridge;
-        this.callHandler = this.callHandler.bind(this);
+    if (callbackid){
+        responseTempOperation(response, callbackid);
     }
-    setupWKWebViewJavascriptBridge(callback) {
-        if (window.WKWebViewJavascriptBridge) {
-            return callback(window.WKWebViewJavascriptBridge);
-        }
-        return null;
-    }
-    handelMessage(e){
-        const { callbackid, response } = e.data || {}
-        const { status } = response || {}
-        if (status && status === 400) {
-            clearInterval();
-            alert('error');
-            return
-        }
+}
 
-        if (callbackid){
-            this.responseTempOperation(response, callbackid);
-        }
-    }
-    responseTempOperation(response, callbackid) {
-        window.WKWVJBCallbacks[callbackid] = response;
-    }
-    async callHandler(methodName, params, callback) {
-        callback(await this.postmessage(methodName,params));
-    }
-    async postmessage(methodName, params) {
-        window.WKWVJBCallbacks = {};
-        const callbackid = new Date().getTime().toString();
-        const message = {
-            params,
-            callbackid,
-            methodName,
-        }
+function responseTempOperation(response, callbackid) {
+    window.WKWVJBCallbacks[callbackid] = response;
+}
 
-        window.top.postMessage(
-            message,
-            window.TARGET_ORIGIN
-        );
-
-        return new Promise((resolve)=>{
-            const timer = setInterval(()=>{
-                if (window.WKWVJBCallbacks[callbackid]){
-                    clearInterval(timer);
-                    resolve(window.WKWVJBCallbacks[callbackid]);
-                }
-            },100);
-        });
+function setupWKWebViewJavascriptBridge(callback) {
+    if (window.WKWebViewJavascriptBridge) {
+        return callback(window.WKWebViewJavascriptBridge);
     }
+    return null;
+}
+
+async function callHandler(methodName, params, callback) {
+    callback(await this.postmessage(methodName,params));
+}
+
+async function postmessage(methodName, params) {
+    window.WKWVJBCallbacks = {};
+    const callbackid = new Date().getTime().toString();
+    const message = {
+        params,
+        callbackid,
+        methodName,
+    }
+
+    window.top.postMessage(
+        message,
+        TARGET_ORIGIN
+    );
+
+    return new Promise((resolve)=>{
+        const timer = setInterval(()=>{
+            if (window.WKWVJBCallbacks[callbackid]){
+                clearInterval(timer);
+                resolve(window.WKWVJBCallbacks[callbackid]);
+            }
+        },100);
+    });
 }
