4c5d45448ca21d638f8d75db68583002b84e772f
Tristan <1286511095@qq.com>
2022-05-24 11:58:39 +0800

    1

---
+++ test.js
@@ -0,0 +1,36 @@
+
+function getToken() {
+    this.setupWKWebViewJavascriptBridge(function (bridge) {
+        let params = {'appId': 'b4933e7b0c12f9c16a'}
+        bridge.callHandler('getMiniProgramToken', params, function(response) {
+            console.log("回来的数据：" + response);
+            if (response) {
+                console.log("##########");
+                console.log(response);
+                const { status, token } = response;
+                document.getElementById('text').innerHTML = token
+            }
+        });
+    });
+}
+
+function removeToken() {
+    this.setupWKWebViewJavascriptBridge(function (bridge) {
+        let params = {'appId': 'b4933e7b0c12f9c16a'}
+        bridge.callHandler('removeMiniProgramToken', params, function(response) {
+            if (response) {
+                console.log(response);
+            }
+        });
+    });
+}
+
+function closeWindow() {
+    this.setupWKWebViewJavascriptBridge(function (bridge) {
+        bridge.callHandler('closePage', null, function(response) {
+            if (response) {
+                console.log(response);
+            }
+        });
+    });
+}
