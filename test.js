3cab0524534d519ba36255b94dd3dcdf6e1b30f5
Tristan <1286511095@qq.com>
2022-05-30 10:58:17 +0800

    1

--- test.js
+++ test.js
@@ -1,24 +1,4 @@
 function getToken() {
-    const img = document.createElement('img')
-    img.src = 'http://127.0.0.1:8082/aaa'
-    document.body.appendChild(img)
+    window.getToken();
 }
 
-
-window.onload = function(){
-    const socket = new WebSocket("ws://localhost:9999");
-
-    socket.onopen = () => {
-        socket.send("Hello! i am aaa");
-    };
-
-    socket.onmessage = (data) => {
-        console.log(data);
-    };
-}
-
-window.onmessage = handelMessage;
-
-function handelMessage(e){
-    console.error(e);
-}
