610e86241c79e8c1910948f233b25db024bf504b
Tristan <1286511095@qq.com>
2022-07-21 16:49:06 +0800

    1

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-function getToken(){
-    window.top.postMessage(
-        'message123',
-        'https://handsomedajiang.github.io',
-    );
-}
+// function getToken(){
+//     window.top.postMessage(
+//         'message123',
+//         'https://handsomedajiang.github.io',
+//     );
+// }
 function getToken1(){
-    // setupWKWebViewJavascriptBridge(function(bridge) {
-    //     bridge.callHandler('getMiniProgramToken',{},function(response) {
-    //        console.log(response);
-    //     });
-    // });
-    window.top.postMessage(
-        'message789797',
-        '*',
-    );
+    setupWKWebViewJavascriptBridge(function(bridge) {
+        bridge.callHandler('getMiniProgramToken',{},function(response) {
+           console.log(response);
+        });
+    });
+    // window.top.postMessage(
+    //     'message789797',
+    //     '*',
+    // );
 }
 
 // window.onmessage = handelMessage;
