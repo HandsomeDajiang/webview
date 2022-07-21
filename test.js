152a44a323968a1d1e9cbfa30d951f9efc9e651b
Tristan <1286511095@qq.com>
2022-07-21 16:35:57 +0800

    1

--- test.js
+++ test.js
@@ -5,8 +5,19 @@ function getToken(){
     //     });
     // });
     window.top.postMessage(
-        'message',
-        'file://*',
+        'message123',
+        'https://handsomedajiang.github.io',
+    );
+}
+function getToken1(){
+    // setupWKWebViewJavascriptBridge(function(bridge) {
+    //     bridge.callHandler('getMiniProgramToken',{},function(response) {
+    //        console.log(response);
+    //     });
+    // });
+    window.top.postMessage(
+        'message789797',
+        '*',
     );
 }
 
