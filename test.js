bcc352f2574f5170c45772d673d830c68bd831ab
Tristan <1286511095@qq.com>
2022-07-22 20:14:50 +0800

    1

--- test.js
+++ test.js
@@ -5,22 +5,32 @@ function getToken(){
             console.error(response);
         });
     });
+}
+
+function removeToken() {
     setupWKWebViewJavascriptBridge(function(bridge) {
         bridge.callHandler('removeMiniProgramToken',{},function(response) {
             console.error('手动移除token');
             console.error(response);
         });
     });
+}
+
+function closePage() {
+    setupWKWebViewJavascriptBridge(function(bridge) {
+        bridge.callHandler('closePage',{},function(response) {
+            console.error('手动关闭window');
+            console.error(response);
+        });
+    });
+}
+
+function setTitle() {
     setupWKWebViewJavascriptBridge(function(bridge) {
         bridge.callHandler('setTitle',{},function(response) {
             console.error('手动设置Title');
             console.error(response);
         });
     });
-    setupWKWebViewJavascriptBridge(function(bridge) {
-        bridge.callHandler('closePage',{},function(response) {
-            console.error('手动关闭窗口');
-            console.error(response);
-        });
-    });
 }
+
